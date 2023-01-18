50ac6c865401eb0ef8c6d86fa0944a2063df87c1
hanyiting <sxyqhyt@126.com>
2023-01-18 10:54:46 +0800

    增加提取关键词功能

--- segment/jieba_segment.py
+++ segment/jieba_segment.py
@@ -1,4 +1,5 @@
 import jieba
+import jieba.analyse
 
 
 # 结巴分词
@@ -20,27 +21,51 @@ class JiebaSegment:
     def suggestfreq(self, word, tune=False):
         jieba.suggest_freq(word, tune)
 
+    # 提取topK词汇
+    def extracttags(self, text, topK=20):
+        return jieba.analyse.extract_tags(text, topK)
+
+    # 获取提取的关键词的权重
+    def textrank(self, text, topK=20, withWeight=True):
+        keyword_dict = {}
+        for x, w in jieba.analyse.textrank(str_text, topK, withWeight=True):
+            keyword_dict[x] = w
+        return keyword_dict
+
 
 if __name__ == '__main__':
     j = JiebaSegment()
 
-    text = '如果放到post中将出错。'
-    test_sent = (
-        "李小福是创新办主任也是云计算方面的专家; 什么是八一双鹿\n"
-        "例如我输入一个带“韩玉赏鉴”的标题，在自定义词库中也增加了此词为N类\n"
-        "「台中」正確應該不會被切開。mac上可分出「石墨烯」；此時又可以分出來凱特琳了。"
-    )
-    result = j.seg(test_sent)
-    print(result)
-    result = j.seg(text, cut_all=False)
-    print(result)
+    # text = '如果放到post中将出错。'
+    # test_sent = (
+    #     "李小福是创新办主任也是云计算方面的专家; 什么是八一双鹿\n"
+    #     "例如我输入一个带“韩玉赏鉴”的标题，在自定义词库中也增加了此词为N类\n"
+    #     "「台中」正確應該不會被切開。mac上可分出「石墨烯」；此時又可以分出來凱特琳了。"
+    # )
+    # result = j.seg(test_sent)
+    # print(result)
+    # result = j.seg(text, cut_all=False)
+    # print(result)
+    #
+    # j.delword("中将")
+    # j.addword("将出错")
+    # result = j.seg(text, cut_all=False)
+    # print(result)
+    #
+    # j.addword("post中")
+    # j.suggestfreq("post中", True)
+    # result = j.seg(text, cut_all=False)
+    # print(result)
 
-    j.delword("中将")
-    j.addword("将出错")
-    result = j.seg(text, cut_all=False)
-    print(result)
+    str_text = "线程（英语：thread" \
+               "）是操作系统能够进行运算调度的最小单位。它被包含在进程之中，是进程中的实际运作单位。一条线程指的是进程中一个单一顺序的控制流，一个进程中可以并发多个线程，每条线程并行执行不同的任务。在Unix " \
+               "System V及SunOS中也被称为轻量进程（lightweight processes），但轻量进程更多指内核线程（kernel thread），而把用户线程（user thread）称为线程。 "
+    # keywords_top1 = j.extracttags(str_text, 10)
+    # print("/".join(keywords_top1))
 
-    j.addword("post中")
-    j.suggestfreq("post中", True)
-    result = j.seg(text, cut_all=False)
-    print(result)
+    print(j.textrank(str_text, topK=10, withWeight=True))
+
+
+
+
+
