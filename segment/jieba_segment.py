382c247c5622faf5e2604271f80a2280546f36a8
hanyiting <sxyqhyt@126.com>
2023-01-18 10:22:59 +0800

    修改结巴分词

--- segment/jieba_segment.py
+++ segment/jieba_segment.py
@@ -11,11 +11,19 @@ class JiebaSegment:
     def seg(self, sentence, cut_all=False):
         return jieba.lcut(sentence, cut_all=cut_all)
 
+    def delword(self, word):
+        jieba.del_word(word)
+
+    def addword(self, word):
+        jieba.add_word(word)
+
+    def suggestfreq(self, word, tune=False):
+        jieba.suggest_freq(word, tune)
 
 if __name__ == '__main__':
     j = JiebaSegment()
 
-    text = '今天天气真好'
+    text = '如果放到post中将出错。'
     test_sent = (
         "李小福是创新办主任也是云计算方面的专家; 什么是八一双鹿\n"
         "例如我输入一个带“韩玉赏鉴”的标题，在自定义词库中也增加了此词为N类\n"
@@ -23,6 +31,16 @@ if __name__ == '__main__':
     )
     result = j.seg(test_sent)
     print(result)
-    # result = j.seg(test_sent, cut_all=True)
-    # print(result)
+    result = j.seg(text, cut_all=False)
+    print(result)
+
+    j.delword("中将")
+    j.addword("将出错")
+    result = j.seg(text, cut_all=False)
+    print(result)
+
+    j.addword("post中")
+    j.suggestfreq("post中", True)
+    result = j.seg(text, cut_all=False)
+    print(result)
 
